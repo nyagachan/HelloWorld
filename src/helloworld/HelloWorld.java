64b2d2cb18e00e4fd8c166eecf6ca4e08a79c4f3
g13931na <g13931na@s30417.tsuda.ac.jp>
2015-04-10 11:09:36 +0900

    Hello,WorldとGoodBye,Worldを追加しました。

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author g13931na
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello,World");
+        System.out.print("こんにちは、世界");
+        System.out.println("GoodBye,World");
+        System.out.print("さようなら、世界");
+    }
+}
